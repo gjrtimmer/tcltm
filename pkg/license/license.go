d06e69dbac3adee6df29d8414b535cf81640d282
G.J.R. Timmer <gjr.timmer@gmail.com>
2019-10-14 18:28:18 +0200

    UPD: license determination and loading

--- pkg/license/license.go
+++ pkg/license/license.go
@@ -1,10 +1,16 @@
 package license
 
 import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/gjrtimmer/tcltm/pkg/config"
+	"github.com/gjrtimmer/tcltm/pkg/resource"
 )
 
 // License represents a module license
@@ -18,27 +24,63 @@ type License struct {
 // if this is a file.
 //
 // A new license is constructed based upon the provided module
-func New(c *config.Config, m *config.Module) *License {
-	l := &License{}
+func New(c *config.Config, m *config.Module) (l *License, err error) {
+	l.content = make([]byte, 0)
 
 	// Determine license type based on config value
 	// First check for a LICENSE file within the InputDirectory
-	if _, err := os.Stat(filepath.Join(c.InputDirectory, "LICENSE")); err == nil {
+	if _, err = os.Stat(filepath.Join(c.InputDirectory, "LICENSE")); err == nil {
 		// LICENSE file present
 		// Load LICENSE file
+		if l.content, err = ioutil.ReadFile(filepath.Join(c.InputDirectory, "LICENSE")); err != nil {
+			return nil, err
+		}
+
+		return l, nil
 	}
 
-	// 1) Check if license is SPDX
-	// 2) Check if license is file
-	// 3) !1 && !2 => License is inline
-	// if spdx, err := SPDXString(m.License); err == nil {
-	// 	// License value is SPDX
-	// 	// Load embedded license
-	// }
+	// Check if license is SPDX
+	if _, err = TemplateString(m.License); err == nil {
+		// License configuration value is a Template
+		//
+		// Load Template
+		r, _ := resource.Get(fmt.Sprintf("/license/%s", m.License))
+		t := template.Must(template.New(m.License).Parse(string(r)))
 
-	//
+		aux := struct {
+			Authors []config.Author
+			Year    int
+		}{
+			Year: time.Now().Year(),
+		}
+		if len(m.Authors) > 0 {
+			aux.Authors = m.Authors
+		}
 
-	return l
+		// Execute Template
+		var b bytes.Buffer
+		if err = t.Execute(&b, aux); err != nil {
+			return nil, err
+		}
+		l.content = b.Bytes()
+		return l, nil
+	}
+
+	// Check if configuration value points to a license file
+	// This value is expected to be relative to the input directory
+	if _, err := os.Stat(filepath.Join(c.InputDirectory, m.License)); err == nil {
+		// Configuration value points to a file.
+		// Load license
+		if l.content, err = ioutil.ReadFile(filepath.Join(c.InputDirectory, m.License)); err != nil {
+			return nil, err
+		}
+		return l, nil
+	}
+
+	// Assume license is inline
+	l.content = []byte(m.License)
+
+	return l, nil
 }
 
 // EOF
